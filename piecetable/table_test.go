2c4b1cd53958af82e491119a41396d5367dbc42e
Rob Fielding <rfielding@pop-os.localdomain>
2020-09-07 12:08:13 -0400

    add load store test

--- piecetable/table_test.go
+++ piecetable/table_test.go
@@ -1,6 +1,7 @@
 package piecetable_test
 
 import (
+	"os"
 	"testing"
 	"github.com/rfielding/teeny/piecetable"
 )
@@ -9,4 +10,18 @@ func TestPtOpen(t *testing.T) {
 	pt := piecetable.New()
 	defer pt.Close()
 	pt.Load("sample.txt")
+	pt.Store("sample.txt.copied.txt")
+
+	stat1, err1 := os.Stat("sample.txt")
+	stat2, err2 := os.Stat("sample.txt.copied.txt")
+
+	if err1 != nil {
+		panic(err1)
+	}
+	if err2 != nil {
+		panic(err2)
+	}
+	if stat1.Size() != stat2.Size() {
+		panic("lengths do not match!")
+	}
 }
